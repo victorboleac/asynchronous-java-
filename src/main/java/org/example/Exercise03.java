0608072fd127d72dc12c2a1d4d973e02dc7cc438
Victor Boleac <victor.boleac@icloud.com>
2024-11-11 12:22:48 +0000

    feat:refact task 3

--- src/main/java/org/example/Exercise03.java
+++ src/main/java/org/example/Exercise03.java
@@ -4,25 +4,16 @@ import java.util.concurrent.CompletableFuture;
 
 public class Exercise03 {
     public static void main(String[] args) {
-        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() ->{
-            try {
-                Thread.sleep(3000);
-                //System.out.print("Hello...");
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-            return "Hello...";
-        });
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> "Hello...");
         CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() ->{
             try {
-                Thread.sleep(5000);
-                //System.out.print("...World!");
+                Thread.sleep(2000);
             }catch (Exception e){
                 e.printStackTrace();
             }
-            return "...World!";
+            return "...World";
         });
-        CompletableFuture<String> combined = future1.thenCombine(future2, (string1, string2)-> string1+string2);
+        CompletableFuture<String> combined = future1.thenCombine(future2, (s1, s2) -> s1+s2);
         combined.thenAccept(System.out::println);
         combined.join();
     }
