f97eef45cce39a95a9b406c9825a4b8d5bdc1a05
Victor Boleac <victor.boleac@icloud.com>
2024-11-11 10:49:27 +0000

    feat: Combining Asynchronous Results

--- src/main/java/org/example/Exercise03.java
+++ src/main/java/org/example/Exercise03.java
@@ -4,22 +4,26 @@ import java.util.concurrent.CompletableFuture;
 
 public class Exercise03 {
     public static void main(String[] args) {
-        CompletableFuture<Void> future1 = CompletableFuture.runAsync(() ->{
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() ->{
             try {
                 Thread.sleep(3000);
-                System.out.print("Hello...");
+                //System.out.print("Hello...");
             }catch (Exception e){
                 e.printStackTrace();
             }
+            return "Hello...";
         });
-        CompletableFuture<Void> future2 = CompletableFuture.runAsync(() ->{
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() ->{
             try {
                 Thread.sleep(5000);
-                System.out.print("...World!");
+                //System.out.print("...World!");
             }catch (Exception e){
                 e.printStackTrace();
             }
+            return "...World!";
         });
-        CompletableFuture.allOf(future1, future2).join();
+        CompletableFuture<String> combined = future1.thenCombine(future2, (string1, string2)-> string1+string2);
+        combined.thenAccept(System.out::println);
+        combined.join();
     }
 }
